f9c39e5602d4f79049745c87e769b5ea85cc472c
assmdx <assmdx183@163.com>
2018-10-18 23:59:37 +0800

    add express server add 路由判断组件AuthRoute

--- reduxdemo/src/index.js
+++ reduxdemo/src/index.js
@@ -9,17 +9,20 @@ import reducers from './reducer'
 import './config'
 import Login from "./container/login/login";
 import Register from "./container/register/register";
+import AuthRoute from './component/authroute/authroute';
 
 const store = createStore(reducers,compose(
     applyMiddleware(thunk),
     window.devToolsExtension?window.devToolsExtension() : f => f
 ))
-
+const Boss = ()=><h1>Boss</h1>
 ReactDom.render(
     (
         <Provider>
             <BrowserRouter>
                 <div>
+                    <AuthRoute></AuthRoute>
+                    <Route path="/boss" component={Boss}></Route>
                     <Route path='/login' component={Login}></Route>
                     <Route path='/register' component={Register}></Route>
                 </div>
