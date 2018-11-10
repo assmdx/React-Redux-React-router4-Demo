1d68558d5a5f88df968d80ed535e7a51319ccb64
assmdx <assmdx183@163.com>
2018-11-10 21:55:17 +0800

    11-2 聊天列表显示

--- reduxdemo/src/component/msg/msg.js
+++ reduxdemo/src/component/msg/msg.js
@@ -1,16 +1,51 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {List} from 'antd-mobile'
+
 @connect(
     state=>state
 )
 class Msg extends React.Component {
+    getLast(arr){
+        return arr[arr.length - 1 ]
+    }
     render(){
+        const userid = this.props.user._id
+        const userinfo = this.props.chat.users
         const msgGroup = {}
+
         this.props.chat.chatmsg.forEach(v=>{
             msgGroup[v.chatid] =msgGroup[v.chatid] || []
             msgGroup[v.chatid].push(v)
         })
-        return (<h1>消息列表</h1>)
+        const chatList = Object.values(msgGroup)
+        console.log(chatList)
+        const Item = List.Item
+        const Brief = Item.Brief
+        return (
+            <div>
+                {
+                    chatList.map(v=>{
+                        const lastItem = this.getLast(v)
+                        const targetId = v[0].from === userid ? v[0].to :v[0].from
+                        const name = userinfo[targetId] ? userinfo[targetId].name : ""
+                        const avatar = userinfo[targetId] ? userinfo[targetId].avatar : ""
+                        return (
+                            <List
+                                key={lastItem._id}
+                            >
+                                <Item
+                                    thumb={require(`../img/${avatar}.png`)}
+                                >
+                                    {lastItem.content}
+                                    <Brief>{name}</Brief>
+                                </Item>
+                            </List>
+                        )}
+                    )
+                }
+            </div>
+        )
     }
 }
 export default Msg
